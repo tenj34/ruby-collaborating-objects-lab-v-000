e32c0cce79fc3f1611c24fafa8b677a5a1adf145
Juan Tenezaca <tenj34@gmail.com>
2018-08-08 19:26:30 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,7 +10,7 @@ class MP3Importer
     @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")} #
   end
 
-  def import
+  def import 
     files.each {|song| Song.new_by_filename(song)}
   end
 end
