fb479e8733f54728a31e88ddc608ca14740a4569
Juan Tenezaca <tenj34@gmail.com>
2018-08-08 19:27:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,8 +7,8 @@ class Song
 
   def self.new_by_filename(file_name)
     #creating a new instance of itself
-    song = file_name.split(" - ")[1]
-    artist = file_name.split(" - ")[0]
+    song = file_name.split(" - ")[1] # split and [1] will equal to the song according to how the mp3's are written
+    artist = file_name.split(" - ")[0] # split and [0] will equal to the artist
     new_song = self.new(song)
     new_song.artist_name = artist
     new_song
