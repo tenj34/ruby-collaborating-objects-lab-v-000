53283b290f6dfdce44c1897c3deab4818aebd52a
Juan Tenezaca <tenj34@gmail.com>
2018-08-08 19:01:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,11 +5,6 @@ class Song
     @name = name
   end
 
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
-  end
-
   def self.new_by_filename(file_name)
     #creating a new instance of itself
     song = file_name.split(" - ")[1]
@@ -18,5 +13,11 @@ class Song
     new_song.artist_name = artist
     new_song
   end
+
+    def artist_name=(name)
+      self.artist = Artist.find_or_create_by_name(name)
+      artist.add_song(self)
+    end
+
 
 end
