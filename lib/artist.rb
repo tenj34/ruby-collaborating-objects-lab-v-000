e6c69d892a69b497bf3f08b3bc07defaea4a61cb
Juan Tenezaca <tenj34@gmail.com>
2018-08-08 19:15:18 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,10 +19,10 @@ class Artist
   def self.all
     @@all
   end
-
+  # must find artist if we find the artist let it be. If not we must create it
+  # found_artist
   def self.find_or_create_by_name(artist_name)
-    # must find artist if we find the artist let it be. If not we must create it
-    # found_artist
+
     found_artist = self.all.find {|artist| artist.name == artist.name} # checking to see if the artists name are the some_filename assign result to found_artist
     if found_artist
       found_artist
